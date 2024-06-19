e93cd6ce7809915b08be0f9eff4ed0f07fb52807
kondyan <arsenij.kondratenko@gmail.com>
2024-06-19 20:15:19 +0200

    task 4 finished

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector(".login-form");
 
+const callback = (ev) => {
+  ev.preventDefault();
+
+  const form = ev.target;
+  const login = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+};
+
+formRef.addEventListener("submit", callback);
